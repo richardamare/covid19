5d02d4d5f9a7d6259846aeac553a99ca4049a4ec
Richard Amare <50375553+RichardAmare@users.noreply.github.com>
2021-05-05 19:35:08 +0200

    Initial Commit

---
+++ CovidCzech/CovidCzechApp.swift
@@ -0,0 +1,17 @@
+//
+//  CovidCzechApp.swift
+//  CovidCzech
+//
+//  Created by Richard Amare on 05.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct CovidCzechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
